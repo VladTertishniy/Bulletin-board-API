18e8793365a14259ee3f72f4cf6f17fc52d88708
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-07-18 15:42:58 +0300

    added ad model for bulletin board

---
+++ src/main/java/com/example/bulletinboardapi/model/Add.java
@@ -0,0 +1,25 @@
+package com.example.bulletinboardapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@Entity(name = "ads")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Add {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdWhen;
+    private LocalDateTime updatedWhen;
+}
