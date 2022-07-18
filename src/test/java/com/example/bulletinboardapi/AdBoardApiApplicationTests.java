f538ae42ac2cb5354533bf5ce94eedc24de2934d
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-07-18 15:45:52 +0300

    refactoring

--- src/test/java/com/example/bulletinboardapi/AdBoardApiApplicationTests.java
+++ src/test/java/com/example/bulletinboardapi/AdBoardApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AddBoardApiApplicationTests {
+class AdBoardApiApplicationTests {
 
 	@Test
 	void contextLoads() {
